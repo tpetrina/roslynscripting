290d4662d2a963349d4b843b014d1bd2109d917f
Toni Petrina <tpetrina@tpetrina.com>
2015-08-12 22:18:56 +0200

    Added simple scripting sample

---
+++ simplescript/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis.Scripting.CSharp;
+using System;
+
+namespace simplescript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var script = CSharpScript.Create("2+2");
+            var result = script.RunAsync();
+
+            result.ReturnValue
+                .ContinueWith(t =>
+                {
+                    Console.WriteLine(result.ReturnValue.Result);
+                });
+
+            Console.ReadKey();
+        }
+    }
+}
