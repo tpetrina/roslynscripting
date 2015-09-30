ca5696bfc697cfc4e75d04edf2c13407848abdb0
Toni Petrina <tpetrina@tpetrina.com>
2015-09-30 16:39:29 +0200

    Added submission check from ScriptCs

--- simplerepl/Program.cs
+++ simplerepl/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis.Scripting;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CodeAnalysis.Scripting.CSharp;
 using System;
 
@@ -6,8 +8,15 @@ namespace simplerepl
 {
     class Program
     {
+        private static CSharpParseOptions options;
+
         static void Main(string[] args)
         {
+            options = new CSharpParseOptions(LanguageVersion.CSharp6,
+                DocumentationMode.Parse,
+                SourceCodeKind.Interactive,
+                null);
+
             Script<object> script = CSharpScript.Create(string.Empty);
 
             while (true)
@@ -20,20 +29,42 @@ namespace simplerepl
                 ScriptState<object> result = null;
                 try
                 {
-                    result = newScript.RunAsync().Result;
-                    script = newScript;
+                    bool isComplete = IsCompleteSubmission(code);
+
+                    if (!isComplete)
+                    {
+                        Console.WriteLine("Incomplete submission");
+                    }
+                    else
+                    {
+                        result = newScript.RunAsync().Result;
+                        script = newScript;
+                    }
                 }
                 catch (Exception ex)
                 {
-                    var color = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine(ex.ToString());
-                    Console.ForegroundColor = color;
+                    var error = ex.ToString();
+                    WriteError(error);
                 }
 
-                if (result != null)
+                if (result != null && result.ReturnValue != null)
                     Console.WriteLine(result.ReturnValue);
             }
+        }
+
+        private static void WriteError(string error)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(error);
+            Console.ForegroundColor = color;
+        }
+
+        private static bool IsCompleteSubmission(string code)
+        {
+            var syntaxTree = SyntaxFactory.ParseSyntaxTree(code, options: options);
+            var isComplete = SyntaxFactory.IsCompleteSubmission(syntaxTree);
+            return isComplete;
         }
     }
 }
