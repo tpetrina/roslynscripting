713a637941c6073cf92d96f8220d4bdd5941b531
Toni Petrina <tpetrina@tpetrina.com>
2015-09-30 14:04:27 +0200

    Updated Roslyn dependencies and added error handling to REPL sample

--- simplerepl/Program.cs
+++ simplerepl/Program.cs
@@ -8,18 +8,31 @@ namespace simplerepl
     {
         static void Main(string[] args)
         {
-            Script<object> script = CSharpScript.Create("");
+            Script<object> script = CSharpScript.Create(string.Empty);
 
             while (true)
             {
                 Console.Write("> ");
                 var code = Console.ReadLine();
 
-                script = script.WithPrevious(script)
-                    .WithCode(code);
-                var result = script.RunAsync().ReturnValue;
+                var newScript = script.ContinueWith(code);
 
-                Console.WriteLine(result.Result);
+                ScriptState<object> result = null;
+                try
+                {
+                    result = newScript.RunAsync().Result;
+                    script = newScript;
+                }
+                catch (Exception ex)
+                {
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(ex.ToString());
+                    Console.ForegroundColor = color;
+                }
+
+                if (result != null)
+                    Console.WriteLine(result.ReturnValue);
             }
         }
     }
